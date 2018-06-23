dfece96f5cbb5d31a6af4a79221e81637f961a5c
Ivan Lopes <ivan (at) 42algoritmos (dot) com (dot) br>
2018-06-23 13:09:26 -0300

    Atualiza o arquivo event_queue.c

--- event_queue.c
+++ event_queue.c
@@ -2,67 +2,67 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-/* Simple queue implemented as a singly linked list with head 
+/* Simple queue implemented as a singly linked list with head
    and tail pointers maintained
 */
 int queue_empty (queue_t q)
 {
-  return q->head == NULL;
+    return q->head == NULL;
 }
 
 queue_t queue_create ()
 {
-  queue_t q;
-  q = malloc (sizeof (struct queue_struct));
-  if (q == NULL)
-    exit (-1);
+    queue_t q;
+    q = malloc (sizeof (struct queue_struct));
+    if (q == NULL)
+        exit (-1);
 
-  q->head = q->tail = NULL;
-  return q;
+    q->head = q->tail = NULL;
+    return q;
 }
 
 void queue_destroy (queue_t q)
 {
-  if (q != NULL)
+    if (q != NULL)
     {
-      while (q->head != NULL)
-	{
-	  queue_entry_t next = q->head;
-	  q->head = next->next_ptr;
-	  next->next_ptr = NULL;
-	  free (next);
-	}
-      q->head = q->tail = NULL;
-      free (q);
+        while (q->head != NULL)
+        {
+            queue_entry_t next = q->head;
+            q->head = next->next_ptr;
+            next->next_ptr = NULL;
+            free (next);
+        }
+        q->head = q->tail = NULL;
+        free (q);
     }
 }
 
 void queue_enqueue (queue_entry_t d, queue_t q)
 {
-  d->next_ptr = NULL;
-  if (q->tail)
+    d->next_ptr = NULL;
+    if (q->tail)
     {
-       q->tail->next_ptr = d;
-       q->tail = d;
+        q->tail->next_ptr = d;
+        q->tail = d;
     }
-  else
+    else
     {
-      q->head = q->tail = d;
+        q->head = q->tail = d;
     }
 }
 
 queue_entry_t  queue_dequeue (queue_t q)
 {
-  queue_entry_t first = q->head;
-  if (first)
+    queue_entry_t first = q->head;
+    if (first)
     {
-      q->head = first->next_ptr;
-      if (q->head == NULL) 
-	{
-	  q->tail = NULL;
-	}
-      first->next_ptr = NULL;
+        q->head = first->next_ptr;
+        if (q->head == NULL)
+        {
+            q->tail = NULL;
+        }
+        first->next_ptr = NULL;
     }
-  return first;
+    return first;
 }
 
